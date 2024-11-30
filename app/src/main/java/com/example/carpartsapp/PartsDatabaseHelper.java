affa5da47303ea8bea06bd3586fde1277e95561c
Carrot <Cobblestone34@gmail.com>
2024-11-29 23:13:28 -0500

    Added a Parts Database

---
+++ app/src/main/java/com/example/carpartsapp/PartsDatabaseHelper.java
@@ -0,0 +1,45 @@
+package com.example.carpartsapp;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.database.sqlite.SQLiteDatabase;
+
+public class PartsDatabaseHelper extends SQLiteOpenHelper {
+
+    private static final String DATABASE_NAME = "PartCollection.db";
+    private static final int DATABASE_VERSION = 1;
+
+    private static final String TABLE_PARTS = "Parts";
+    private static final String COLUMN_ID = "part_id";
+    private static final String COLUMN_NAME = "name";
+    private static final String COLUMN_DISTRIBUTOR = "distributor";
+    private static final String COLUMN_DESCRIPTION = "description";
+    private static final String COLUMN_PRICE = "price";
+
+
+
+    public PartsDatabaseHelper(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        String createTable = "CREATE TABLE " + TABLE_PARTS + " (" +
+                COLUMN_ID + " INTEGER PRIMARY KEY AUTOINCREMENT, " +
+                COLUMN_NAME + " TEXT, " +
+                COLUMN_DISTRIBUTOR + " TEXT, " +
+                COLUMN_DESCRIPTION + " TEXT," +
+                COLUMN_PRICE + " REAL)";
+        db.execSQL(createTable);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL("DROP TABLE IF EXISTS " + TABLE_PARTS);
+        onCreate(db);
+
+
+    }
+
+}
+
--- app/src/main/java/com/example/carpartsapp/PartsMenu.java
+++ app/src/main/java/com/example/carpartsapp/PartsMenu.java
@@ -6,6 +6,9 @@ import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class PartsMenu extends AppCompatActivity {
+    PartsDatabaseHelper PartsDb;
+
+
 
     private SharedPreferences sharedPreferences;
 
@@ -13,6 +16,8 @@ public class PartsMenu extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.parts_menu);
+
+        PartsDb = new PartsDatabaseHelper(this);
 
         sharedPreferences = getSharedPreferences("CartSummary", MODE_PRIVATE);
 
